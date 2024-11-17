399ac475324c7e506c88e95c182d2c63ee8e5105
KleoVor <t9671253993@gmail.com>
2024-11-17 14:55:46 +0300

    test

--- app/src/androidTest/kotlin/ru/netology/testing/uiautomator/ChangeTextTest.kt
+++ app/src/androidTest/kotlin/ru/netology/testing/uiautomator/ChangeTextTest.kt
@@ -6,111 +6,108 @@ import androidx.test.ext.junit.runners.AndroidJUnit4
 import androidx.test.platform.app.InstrumentationRegistry
 import androidx.test.uiautomator.By
 import androidx.test.uiautomator.UiDevice
-import androidx.test.uiautomator.UiSelector
 import androidx.test.uiautomator.Until
 import org.junit.Assert.assertEquals
 import org.junit.Before
 import org.junit.Test
 import org.junit.runner.RunWith
 
+// Константы для использования в тестах
+const val SETTINGS_PACKAGE = "com.android.settings" // Пакет настроек Android
+const val MODEL_PACKAGE = "ru.netology.testing.uiautomator" // Пакет под тестирование
+const val TIMEOUT = 5000L // Время ожидания в миллисекундах
 
-const val SETTINGS_PACKAGE = "com.android.settings"
-const val MODEL_PACKAGE = "ru.netology.testing.uiautomator"
-
-const val TIMEOUT = 5000L
-
+// Указываем, что тесты будут выполнены с использованием AndroidJUnit4
 @RunWith(AndroidJUnit4::class)
 class ChangeTextTest {
 
-    private lateinit var device: UiDevice
-    private val textToSet = "Netology"
+    private lateinit var device: UiDevice // Объект для взаимодействия с устройством
+    private val textToSet = "Netology" // Текст для проверки корректного ввода
+    private val emptyText = "" // Пустая строка для тестов
+    private val textToSet1 = "     " // Строка, состоящая только из пробелов
 
-//    @Test
-//    fun testInternetSettings() {
-//        // Press home
-//        device = UiDevice.getInstance(InstrumentationRegistry.getInstrumentation())
-//        device.pressHome()
-//
-//        // Wait for launcher
-//        val launcherPackage = device.launcherPackageName
-//        device.wait(Until.hasObject(By.pkg(launcherPackage)), TIMEOUT)
-//        waitForPackage(SETTINGS_PACKAGE)
-//
-//        val context = ApplicationProvider.getApplicationContext<Context>()
-//        val intent = context.packageManager.getLaunchIntentForPackage(SETTINGS_PACKAGE)
-//        context.startActivity(intent)
-//        device.wait(Until.hasObject(By.pkg(SETTINGS_PACKAGE)), TIMEOUT)
-//
-//        device.findObject(
-//            UiSelector().resourceId("android:id/title").instance(0)
-//        ).click()
-//    }
-
-//    @Test
-//    fun testChangeText() {
-//        // Press home
-//        device = UiDevice.getInstance(InstrumentationRegistry.getInstrumentation())
-//        device.pressHome()
-//
-//        // Wait for launcher
-//        val launcherPackage = device.launcherPackageName
-//        device.wait(Until.hasObject(By.pkg(launcherPackage)), TIMEOUT)
-//        waitForPackage(SETTINGS_PACKAGE)
-//
-//        val context = ApplicationProvider.getApplicationContext<Context>()
-//        val packageName = context.packageName
-//        val intent = context.packageManager.getLaunchIntentForPackage(packageName)
-//        context.startActivity(intent)
-//        device.wait(Until.hasObject(By.pkg(packageName)), TIMEOUT)
-//
-//
-//        device.findObject(By.res(packageName, "userInput")).text = textToSet
-//        device.findObject(By.res(packageName, "buttonChange")).click()
-//
-//        val result = device.findObject(By.res(packageName, "textToBeChanged")).text
-//        assertEquals(result, textToSet)
-//    }
-
+    // Метод для ожидания, пока пакет приложения станет доступен
     private fun waitForPackage(packageName: String) {
-        val context = ApplicationProvider.getApplicationContext<Context>()
-        val intent = context.packageManager.getLaunchIntentForPackage(packageName)
-        context.startActivity(intent)
-        device.wait(Until.hasObject(By.pkg(packageName)), TIMEOUT)
+        val context = ApplicationProvider.getApplicationContext<Context>() // Получаем контекст приложения
+        val intent = context.packageManager.getLaunchIntentForPackage(packageName) // Получаем интент для запуска приложения
+        context.startActivity(intent) // Запускаем приложение
+        device.wait(Until.hasObject(By.pkg(packageName)), TIMEOUT) // Ждем, пока приложение станет доступно
     }
 
+    // Подготовка перед каждым тестом: инициализация устройства и ожидание запуска лаунчера
     @Before
-    fun beforeEachTest() {
-        // Press home
-        device = UiDevice.getInstance(InstrumentationRegistry.getInstrumentation())
-        device.pressHome()
-
-        // Wait for launcher
-        val launcherPackage = device.launcherPackageName
-        device.wait(Until.hasObject(By.pkg(launcherPackage)), TIMEOUT)
+    fun setUp() {
+        device = UiDevice.getInstance(InstrumentationRegistry.getInstrumentation()) // Получаем экземпляр UiDevice
+        device.pressHome() // Нажимаем кнопку Home
+        val launcherPackage = device.launcherPackageName // Получаем имя пакета лаунчера
+        device.wait(Until.hasObject(By.pkg(launcherPackage)), TIMEOUT) // Ждем, пока лаунчер станет доступным
     }
 
-    @Test
-    fun testInternetSettings() {
-        waitForPackage(SETTINGS_PACKAGE)
-
-        device.findObject(
-            UiSelector().resourceId("android:id/title").instance(0)
-        ).click()
-    }
-
-    @Test
-    fun testChangeText() {
+    // Метод для ввода текста и нажатия кнопки изменения текста
+    private fun inputTextAndClick(userInput: String): String {
         val packageName = MODEL_PACKAGE
         waitForPackage(packageName)
 
-        device.findObject(By.res(packageName, "userInput")).text = textToSet
+        // Вводим текст в поле ввода
+        device.findObject(By.res(packageName, "userInput")).text = userInput
         device.findObject(By.res(packageName, "buttonChange")).click()
+        // Возвращаем текст, который отображается в элементе textToBeChanged
+        return device.findObject(By.res(packageName, "textToBeChanged")).text
+    }
 
-        val result = device.findObject(By.res(packageName, "textToBeChanged")).text
+    // Метод для получения текущего текста из элемента textToBeChanged
+    private fun getCurrentText(): String {
+        val packageName = MODEL_PACKAGE
+        waitForPackage(packageName)
+        // Возвращаем текст из элемента textToBeChanged
+        return device.findObject(By.res(packageName, "textToBeChanged")).text
+    }
+
+    // Метод для ввода текста и открытия нового Activity
+    private fun inputTextAndOpenNewActivity(inputText: String) {
+        val packageName = MODEL_PACKAGE
+        waitForPackage(packageName)
+
+        // Вводим текст в поле ввода
+        device.findObject(By.res(packageName, "userInput")).text = inputText
+        device.findObject(By.res(packageName, "buttonActivity")).click()
+
+        // Ждем, пока новое Activity откроется
+        device.wait(Until.hasObject(By.res(packageName, "text")), TIMEOUT)
+    }
+
+    // Тестирование: пустой ввод не должен изменять текст
+    @Test
+    fun testEmptyTextInputDoesNotChangeOutput() {
+        val initialResult = getCurrentText()
+        val result = inputTextAndClick(emptyText)
+
+        assertEquals(initialResult, result)
+    }
+
+    // Тестирование: корректный ввод текста должен изменять текст
+    @Test
+    fun testValidTextInputChangesOutput() {
+        val result = inputTextAndClick(textToSet)
         assertEquals(result, textToSet)
     }
 
+    // Тестирование: изменение текста и открытие нового Activity
+    @Test
+    fun testChangeTextAndOpenNewActivity() {
+        val inputText = textToSet
+        inputTextAndOpenNewActivity(inputText)
+        val resultText = device.findObject(By.res(MODEL_PACKAGE, "text")).text
+        assertEquals(inputText, resultText)
+    }
+
+    // Тестирование: попытка изменить текст на пустую строку и открыть новое Activity
+    @Test
+    fun testChangeTextEmptyTextAndOpenNewActivity() {
+        val inputText = textToSet1
+        inputTextAndOpenNewActivity(inputText)
+
+        val resultText = device.findObject(By.res(MODEL_PACKAGE, "text")).text
+        assertEquals(inputText, resultText)
+    }
 }
-
-
-
